edce0da672dc69ec88d1e452f72cb842cd0ad016
Thomas Dale <thomasdale92@gmail.com>
2019-05-19 21:14:25 +0100

    Completed lecture 22: Strings

---
+++ Strings/src/com/tomdale/Main.java
@@ -0,0 +1,19 @@
+package com.tomdale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myString = "This is a String";
+        System.out.println("This is my String =  " + "'" + myString + "'");
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+        double doubleNumber = 120.47;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString value: " + lastString);
+
+
+    }
+}
