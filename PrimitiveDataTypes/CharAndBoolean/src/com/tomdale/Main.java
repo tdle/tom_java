c653080c1e94e90137a2c9912ccffa9878be3721
Thomas Dale <thomasdale92@gmail.com>
2019-05-19 20:39:37 +0100

    completed lecture 21 - Char and Boolean

---
+++ PrimitiveDataTypes/CharAndBoolean/src/com/tomdale/Main.java
@@ -0,0 +1,10 @@
+package com.tomdale;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char registeredSymbol = '\u00AE';
+        System.out.println(registeredSymbol);
+    }
+}
