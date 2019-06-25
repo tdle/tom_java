3779039f7034b91a2a63143237c17299e5c5c60e
Thomas Dale <thomasdale92@gmail.com>
2019-06-25 16:42:12 +0100

    Completed Methods part 1

---
+++ Methods/src/com/tomdale/Main.java
@@ -0,0 +1,26 @@
+package com.tomdale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score " + finalScore);
+            return finalScore;
+        } else {
+            return -1;
+        }
+    }
+}
